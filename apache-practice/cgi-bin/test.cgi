3dcfdb411eb27bd11fd9887f5e8e3d4916354107
sunakan <sunakan03@gmail.com>
2020-06-07 05:31:59 +0000

    apacheの簡単な練習を追加

---
+++ apache-practice/cgi-bin/test.cgi
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+print "Content-Type:text/html; charset=UTF-8\n";
+print "\n";
+print "<html>\n";
+print "<body>";
+print "<h1>はじめてのCGI</h1>";
+print "</body></html>\n";
---
+++ apache-practice/html/test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>HTML5サンプル</title>
+  </head>
+  <body>
+  <?php
+    echo "<h1>PHP test</h1>";
+    phpinfo();
+  ?>
+  </body>
+</html>
